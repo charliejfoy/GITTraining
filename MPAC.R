6d49c7468975483139cb5f557379dfb52d2e94f5
Charlie Foy <charliejfoy@gmail.com>
2016-09-15 10:09:29 -0400

    This is a terrible change, I shouldn't really comnit this

--- MPAC.R
+++ MPAC.R
@@ -1,11 +1,16 @@
-# Read in a text file and make sure that the first column is character
-# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
-column_classes=c("character","numeric","character")
-data<-read.csv("validate-vars.txt", colClasses=column_classes)
 
-# Freq 
+
+
+
+# Aggregate by ID
 # Compare Means
 # Cross Tabs
+# Freq asdfoiashdfasdf
+# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
+# Read in a text file and make sure that the first column is character
 # Validate variables
-# Aggregate by ID
-
+'poa4ur]]
+as[9u430nhj'ieogf
+asojfsad[ojfas
+column_classes=c("character","numeric","character"8qh')
+data<-read.csv("validate-adsf;oiafvk;oidsa [soifdj a'slfaars.txt", colClasses=column_classes)
