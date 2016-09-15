d5863d89452e8258c32b72b47e1207462ec0a761
SH <shannon.handsor@mpac.ca>
2016-09-15 12:03:19 -0400

    Shannon's edits

--- MPAC.R
+++ MPAC.R
@@ -9,3 +9,8 @@ data<-read.csv("validate-vars.txt", colClasses=column_classes)
 # Validate variables
 # Aggregate by ID
 
+# only print rows that have a valid variable in "Var": A,B,C,D (not E)
+
+# http://stackoverflow.com/questions/1686569/filtering-a-data-frame-in-r
+
+validdata<-data[data$Var %in% c("A","B","C","D"),]
