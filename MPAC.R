dbf00d3c5530ae112c6ee9aa4f2447d0cd0783b7
Charlie Foy <charliejfoy@gmail.com>
2016-09-15 09:49:32 -0400

    Add in the syntax to read in a file

--- MPAC.R
+++ MPAC.R
@@ -1,5 +1,11 @@
+# Read in a text file and make sure that the first column is character
+# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
+column_classes=c("character","numeric","character")
+data<-read.csv("validate-vars.txt", colClasses=column_classes)
+
 # Freq 
 # Compare Means
 # Cross Tabs
 # Validate variables
 # Aggregate by ID
+
